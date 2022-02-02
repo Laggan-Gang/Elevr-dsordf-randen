5b053dbfbfeaaf82f9b8bfb8d568595935564f13
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-02-03 00:53:52 +0100

    Update elevis.js

--- elevis.js
+++ elevis.js
@@ -53,158 +53,164 @@ client.on("messageCreate", async (meddelande) => {
   let resten = meddelande.content.slice(5);
   if (!meddelande.author.bot) {
     if (meddelande.type === "REPLY") {
-      let brottsling = meddelande.mentions.repliedUser;
-      let brottet = await meddelande.channel.messages.fetch(
-        meddelande.reference.messageId
-      );
-      var bevis = "";
-      if (brottet.content !== "") {
-        bevis = '"' + brottet.content + '" ';
-      }
-      if (dravel.startsWith("varning")) {
-        brottet.reply(
-          "Det här är INTE acceptabelt beteende " +
-            meddelande.mentions.repliedUser.toString() +
-            "! Du kan se det här som en formell varning och jag hoppas *innerligt* att du funderar både en och två gånger innan du postar " +
-            bevis +
-            "igen!"
-        );
-      } else if (dravel.startsWith("warning")) {
-        brottet.reply(
-          "This is NOT acceptable behaviour " +
-            meddelande.mentions.repliedUser.toString() +
-            "! You can consider this a formal warning and I *sincerely* hope you will think twice about posting " +
-            bevis +
-            "again!"
-        );
-      }
-    }
-    if (dravel.startsWith("roll")) {
-      if (meddelande.channel.id == "539847809004994560") {
-        let role = meddelande.guild.roles.cache.find(
-          (role) => role.name === resten
-        );
-        if (role == undefined) {
-          meddelande.reply(
-            "Finner inget sådant sällskap i den här kanalen, har du provat bing.com? Kom ihåg att jag är en viktigpetter och är väldigt noggrann med gemener och versaler :)"
-          );
-        } else {
-          console.log(role);
-          if (!meddelande.member.roles.cache.some((r) => r.name === resten)) {
-            meddelande.member.roles
-              .add(role)
-              .then(() => {
-                meddelande.reply(
-                  "Har talat med fakulteten och har beslutat att gå din förfrågan till mötes. Välkomen till " +
-                    role.toString() +
-                    "-klubben!"
-                );
-              })
-              .catch((error) => {
-                if (
-                  error.toString() === "DiscordAPIError: Missing Permissions"
-                ) {
-                  meddelande.reply(
-                    "Har undersökt saken och kommit fram till att du inte riktigt är den typ vi söker som " +
-                      role.toString()
-                  );
-                }
-              });
-          } else {
-            meddelande.reply(
-              "Efter eftertanke har det slagit mig att du redan är " +
-                role.toString() +
-                "! Jag kan tyvärr inte göra dig till dubbel-" +
-                role.toString() +
-                " :("
-            );
-          }
-        }
-      } else {
-        meddelande.reply(
-          "Här skulle jag vilja kliva in och påminna dig om att digital nedskräpning är fortfarande nedskrpäning, och att ägna sig åt sådanna vedervärdiga projekt gör hela kanalen till åtlöje. Vänligen se till att alla dina icke-essentiella bot-kommandon skrivs i " +
-            meddelande.guild.channels.cache
-              .get("539847809004994560")
-              .toString() +
-            ". Fundera ett slag över dina handlingar och hur de påverkar de i din omgivning. Varning är ett bra exempel på ett essentiellt bot-kommando."
-        );
-      }
-    } else if (dravel.startsWith("role")) {
-      if (meddelande.channel.id == "539847809004994560") {
-        let role = meddelande.guild.roles.cache.find(
-          (role) => role.name === resten
-        );
-        if (role == undefined) {
-          meddelande.reply(
-            "I find no such company in this channel, perhaps you would like to try yandex.ru? Bear in mind that I am an important petter and meticulous about upper- and lowercase :)"
-          );
-        } else {
-          console.log(role);
-          if (!meddelande.member.roles.cache.some((r) => r.name === resten)) {
-            meddelande.member.roles
-              .add(role)
-              .then(() => {
-                meddelande.reply(
-                  "I have spoken with the faculty and made the descision to grant your request. Welcome to the " +
-                    role.toString() +
-                    "-society!"
-                );
-              })
-              .catch((whoops) => {
-                if (
-                  whoops.toString() === "DiscordAPIError: Missing Permissions"
-                ) {
-                  meddelande.reply(
-                    "After some deep consideration I have reached the conclusion that you are not the kind of person we are looking for as " +
-                      role.toString()
-                  );
-                }
-              });
-          } else {
-            meddelande.reply(
-              "Thinking about it now, I have come to realize that you already are " +
-                role.toString() +
-                "! Unfortunately, I cannot make you double-" +
-                role.toString() +
-                " :("
-            );
-          }
-        }
-      } else {
-        meddelande.reply(
-          "I would like to inform and remind you the technological littering is still littering, and by indulging in such vile activities you are tarnishing the sanctity of the channel. Please always make sure to post non-essential bot commands in" +
-            meddelande.guild.channels.cache
-              .get("539847809004994560")
-              .toString() +
-            ". Try to think about your actions and how they affect those around you. Warning other player is a good example of an essential bot command."
-        );
-      }
+      elevRådsOrdförande(meddelande, dravel);
     } else if (dravel === "pang!") {
       if (meddelande.member.voice.channel !== null) {
-        let channel = meddelande.member.voice.channel;
-        const player = createAudioPlayer();
-        const resource = createAudioResource(
-          "clickclackmotherfuckerthegunscomingoutyougottreesecondsFIVE.wav"
+        await clickclackmotherfuckerthegunscomingoutyougottreesecondsFIVE(
+          meddelande
         );
-        const connection = joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-        const subscription = connection.subscribe(player);
-        player.play(resource);
-        await löftesKollaren(player);
-        if (subscription) {
-          subscription.unsubscribe();
-          connection.destroy();
-          player.stop();
-        }
       } else {
         meddelande.reply("?");
       }
     }
   }
 });
+
+async function clickclackmotherfuckerthegunscomingoutyougottreesecondsFIVE(
+  meddelande
+) {
+  let channel = meddelande.member.voice.channel;
+  const player = createAudioPlayer();
+  const resource = createAudioResource(
+    "clickclackmotherfuckerthegunscomingoutyougottreesecondsFIVE.wav"
+  );
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+  const subscription = connection.subscribe(player);
+  player.play(resource);
+  await löftesKollaren(player);
+  if (subscription) {
+    subscription.unsubscribe();
+    connection.destroy();
+    player.stop();
+  } else {
+    meddelande.reply("?");
+  }
+}
+
+function elevRådsOrdförande(meddelande, dravel) {
+  let brottsling = meddelande.mentions.repliedUser;
+  let brottet = await meddelande.channel.messages.fetch(
+    meddelande.reference.messageId
+  );
+  var bevis = "";
+  if (brottet.content !== "") {
+    bevis = '"' + brottet.content + '" ';
+  }
+  if (dravel.startsWith("varning")) {
+    brottet.reply(
+      "Det här är INTE acceptabelt beteende " +
+        meddelande.mentions.repliedUser.toString() +
+        "! Du kan se det här som en formell varning och jag hoppas *innerligt* att du funderar både en och två gånger innan du postar " +
+        bevis +
+        "igen!"
+    );
+  } else if (dravel.startsWith("warning")) {
+    brottet.reply(
+      "This is NOT acceptable behaviour " +
+        meddelande.mentions.repliedUser.toString() +
+        "! You can consider this a formal warning and I *sincerely* hope you will think twice about posting " +
+        bevis +
+        "again!"
+    );
+  }
+}
+if (dravel.startsWith("roll")) {
+  if (meddelande.channel.id == "539847809004994560") {
+    let role = meddelande.guild.roles.cache.find(
+      (role) => role.name === resten
+    );
+    if (role == undefined) {
+      meddelande.reply(
+        "Finner inget sådant sällskap i den här kanalen, har du provat bing.com? Kom ihåg att jag är en viktigpetter och är väldigt noggrann med gemener och versaler :)"
+      );
+    } else {
+      console.log(role);
+      if (!meddelande.member.roles.cache.some((r) => r.name === resten)) {
+        meddelande.member.roles
+          .add(role)
+          .then(() => {
+            meddelande.reply(
+              "Har talat med fakulteten och har beslutat att gå din förfrågan till mötes. Välkomen till " +
+                role.toString() +
+                "-klubben!"
+            );
+          })
+          .catch((error) => {
+            if (error.toString() === "DiscordAPIError: Missing Permissions") {
+              meddelande.reply(
+                "Har undersökt saken och kommit fram till att du inte riktigt är den typ vi söker som " +
+                  role.toString()
+              );
+            }
+          });
+      } else {
+        meddelande.reply(
+          "Efter eftertanke har det slagit mig att du redan är " +
+            role.toString() +
+            "! Jag kan tyvärr inte göra dig till dubbel-" +
+            role.toString() +
+            " :("
+        );
+      }
+    }
+  } else {
+    meddelande.reply(
+      "Här skulle jag vilja kliva in och påminna dig om att digital nedskräpning är fortfarande nedskrpäning, och att ägna sig åt sådanna vedervärdiga projekt gör hela kanalen till åtlöje. Vänligen se till att alla dina icke-essentiella bot-kommandon skrivs i " +
+        meddelande.guild.channels.cache.get("539847809004994560").toString() +
+        ". Fundera ett slag över dina handlingar och hur de påverkar de i din omgivning. Varning är ett bra exempel på ett essentiellt bot-kommando."
+    );
+  }
+} else if (dravel.startsWith("role")) {
+  if (meddelande.channel.id == "539847809004994560") {
+    let role = meddelande.guild.roles.cache.find(
+      (role) => role.name === resten
+    );
+    if (role == undefined) {
+      meddelande.reply(
+        "I find no such company in this channel, perhaps you would like to try yandex.ru? Bear in mind that I am an important petter and meticulous about upper- and lowercase :)"
+      );
+    } else {
+      console.log(role);
+      if (!meddelande.member.roles.cache.some((r) => r.name === resten)) {
+        meddelande.member.roles
+          .add(role)
+          .then(() => {
+            meddelande.reply(
+              "I have spoken with the faculty and made the descision to grant your request. Welcome to the " +
+                role.toString() +
+                "-society!"
+            );
+          })
+          .catch((whoops) => {
+            if (whoops.toString() === "DiscordAPIError: Missing Permissions") {
+              meddelande.reply(
+                "After some deep consideration I have reached the conclusion that you are not the kind of person we are looking for as " +
+                  role.toString()
+              );
+            }
+          });
+      } else {
+        meddelande.reply(
+          "Thinking about it now, I have come to realize that you already are " +
+            role.toString() +
+            "! Unfortunately, I cannot make you double-" +
+            role.toString() +
+            " :("
+        );
+      }
+    }
+  } else {
+    meddelande.reply(
+      "I would like to inform and remind you the technological littering is still littering, and by indulging in such vile activities you are tarnishing the sanctity of the channel. Please always make sure to post non-essential bot commands in" +
+        meddelande.guild.channels.cache.get("539847809004994560").toString() +
+        ". Try to think about your actions and how they affect those around you. Warning other player is a good example of an essential bot command."
+    );
+  }
+}
 
 // Login to Discord with your client's token this should always go last I guess?
 client.login(token);
