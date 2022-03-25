7b78cd719fa8fd85ed67cf7e130f75b96be85fa3
Todor Todorov <todor.o.todorov93@gmail.com>
2022-03-25 17:01:39 +0000

    feat: add calcuateDotaWiener function to calculate the calculation of dota winrate for a single player

---
+++ wienerchickendinner.js
@@ -0,0 +1,32 @@
+const { laggStatsBaseUrl } = require("./config.json");
+const axios = require("axios");
+//@ts-check
+
+async function getPlayerStats(playerId, gameId) {
+  const request = {
+    baseURL: laggStatsBaseUrl,
+    url: "result/all",
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    responseType: "json",
+  };
+  const response = await axios(request);
+
+  return response.data.filter(
+    (r) => r.game === gameId && r.username === playerId
+  );
+}
+
+async function calculateDotaWiener(playerId) {
+  const playerStats = await getPlayerStats(playerId, "Dota 2");
+
+  const wins = playerStats.filter((r) => r.win).length;
+
+  return (wins / playerStats.length) * 100;
+}
+
+module.exports = {
+    calculateDotaWiener
+}
