fdf04de051b9760327f825225ef2fe694c096d34
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-04-14 17:25:14 +0200

    test

--- statCollector.js
+++ statCollector.js
@@ -166,8 +166,10 @@ module.exports = {
   //games.forEach((game) => {
   //  exampleEmbed.addField("", `${game}`, false);
   //});
-    const sortGameList = games.map((g, index) => ({`${index + 1}. ${g}`})
+    const sortGameList = games.map((g, index) => ({`${index + 1}. ${g}`}));
   exampleEmbed.addField("I have consulted the archives and this is what I got:", `${sortGameList.join("\n")}`, true);
+  
+
  
 
     //const embed = new MessageEmbed()
